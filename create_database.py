3edacc5ba2c5d4f599fb123f9ac2763bb5346331
alvaroaira14 <a.aira.quiroga@gmail.com>
2023-05-30 18:57:19 +0200

    create database code

--- create_database.py
+++ create_database.py
@@ -1,27 +1,30 @@
-try:
-    import pymysql
-except ModuleNotFoundError:
-    print(\"pymysql module not found. Installing...\")
-    !pip install pymysql
-    import pymysql
+def create_database():
+    try:
+        import pymysql
 
-host = 'localhost'
-user = 'tfm_user'
-password = 'tfmdatabase1234'
-database = 'tfm_database2'
+    except ModuleNotFoundError:
+        import pip
+        pip.main(['install', 'pymysql'])
+        import pymysql
 
-connection = pymysql.connect(host=host, user=user, password=password)
-schema = 
-        """CREATE TABLE IF NOT EXISTS employees (,
-            id INT AUTO_INCREMENT PRIMARY KEY,,
-            name VARCHAR(100),",
-            age INT,",
-            salary FLOAT");"""
 
-try:
-    connection.select_db(database)
-    cursor = connection.cursor()
-    cursor.execute(schema)
+    host = 'localhost'
+    user = 'tfm_user'
+    password = 'tfmdatabase1234'
+    database = 'tfm_database'
 
-except pymysql.OperationalError:
-    print("No existe la BBDD")
+    connection = pymysql.connect(host=host, user=user, password=password)
+
+    try:
+        connection.select_db(database)
+        cursor = connection.cursor()
+
+        with open('database_schema.sql', 'r') as schema:
+            database_schema = schema.read()
+
+        cursor.execute(database_schema)
+        connection.commit()
+
+
+    except pymysql.OperationalError:
+        print("No existe la BBDD")
