2fcfaf8806e5300982065f039d8e77a55aeb1f4f
alvaroaira14 <a.aira.quiroga@gmail.com>
2023-09-02 13:06:38 +0200

    cambios en los logs

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     try:
         os.system(r"myenv\Scripts\activate")
         os.system("pip install -r requirements.txt")
-        print("\n--Paquetes instalados correctamente--")
+        print("\n--Paquetes instalados correctamente--\n")
     except Exception as e:
         print("An error occurred while installing packages:", e)
 
